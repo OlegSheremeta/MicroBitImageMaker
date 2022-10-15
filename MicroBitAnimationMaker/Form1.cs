893b41f985b65936e75452aa98bd2d51a9971c6b
oleg0 <oleg0@DESKTOP-DN7M09B>
2022-10-15 12:30:01 +0300

    Добавьте файлы проекта.

---
+++ MicroBitAnimationMaker/Form1.cs
@@ -0,0 +1,392 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MicroBitAnimationMaker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button thisBtn = (Button)sender;
+            if(thisBtn.Text == ".")
+            {
+                thisBtn.Text = "#";
+            } else if(thisBtn.Text == "#")
+            {
+                thisBtn.Text = ".";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button thisBtn = (Button)sender;
+            if (thisBtn.Text == ".")
+            {
+                thisBtn.Text = "#";
+            }
+            else if (thisBtn.Text == "#")
+            {
+                thisBtn.Text = ".";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Button thisBtn = (Button)sender;
+            if (thisBtn.Text == ".")
+            {
+                thisBtn.Text = "#";
+            }
+            else if (thisBtn.Text == "#")
+            {
+                thisBtn.Text = ".";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Button thisBtn = (Button)sender;
+            if (thisBtn.Text == ".")
+            {
+                thisBtn.Text = "#";
+            }
+            else if (thisBtn.Text == "#")
+            {
+                thisBtn.Text = ".";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Button thisBtn = (Button)sender;
+            if (thisBtn.Text == ".")
+            {
+                thisBtn.Text = "#";
+            }
+            else if (thisBtn.Text == "#")
+            {
+                thisBtn.Text = ".";
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Button thisBtn = (Button)sender;
+            if (thisBtn.Text == ".")
+            {
+                thisBtn.Text = "#";
+            }
+            else if (thisBtn.Text == "#")
+            {
+                thisBtn.Text = ".";
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Button thisBtn = (Button)sender;
+            if (thisBtn.Text == ".")
+            {
+                thisBtn.Text = "#";
+            }
+            else if (thisBtn.Text == "#")
+            {
+                thisBtn.Text = ".";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Button thisBtn = (Button)sender;
+            if (thisBtn.Text == ".")
+            {
+                thisBtn.Text = "#";
+            }
+            else if (thisBtn.Text == "#")
+            {
+                thisBtn.Text = ".";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Button thisBtn = (Button)sender;
+            if (thisBtn.Text == ".")
+            {
+                thisBtn.Text = "#";
+            }
+            else if (thisBtn.Text == "#")
+            {
+                thisBtn.Text = ".";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Button thisBtn = (Button)sender;
+            if (thisBtn.Text == ".")
+            {
+                thisBtn.Text = "#";
+            }
+            else if (thisBtn.Text == "#")
+            {
+                thisBtn.Text = ".";
+            }
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            Button thisBtn = (Button)sender;
+            if (thisBtn.Text == ".")
+            {
+                thisBtn.Text = "#";
+            }
+            else if (thisBtn.Text == "#")
+            {
+                thisBtn.Text = ".";
+            }
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            Button thisBtn = (Button)sender;
+            if (thisBtn.Text == ".")
+            {
+                thisBtn.Text = "#";
+            }
+            else if (thisBtn.Text == "#")
+            {
+                thisBtn.Text = ".";
+            }
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            Button thisBtn = (Button)sender;
+            if (thisBtn.Text == ".")
+            {
+                thisBtn.Text = "#";
+            }
+            else if (thisBtn.Text == "#")
+            {
+                thisBtn.Text = ".";
+            }
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Button thisBtn = (Button)sender;
+            if (thisBtn.Text == ".")
+            {
+                thisBtn.Text = "#";
+            }
+            else if (thisBtn.Text == "#")
+            {
+                thisBtn.Text = ".";
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Button thisBtn = (Button)sender;
+            if (thisBtn.Text == ".")
+            {
+                thisBtn.Text = "#";
+            }
+            else if (thisBtn.Text == "#")
+            {
+                thisBtn.Text = ".";
+            }
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            Button thisBtn = (Button)sender;
+            if (thisBtn.Text == ".")
+            {
+                thisBtn.Text = "#";
+            }
+            else if (thisBtn.Text == "#")
+            {
+                thisBtn.Text = ".";
+            }
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            Button thisBtn = (Button)sender;
+            if (thisBtn.Text == ".")
+            {
+                thisBtn.Text = "#";
+            }
+            else if (thisBtn.Text == "#")
+            {
+                thisBtn.Text = ".";
+            }
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            Button thisBtn = (Button)sender;
+            if (thisBtn.Text == ".")
+            {
+                thisBtn.Text = "#";
+            }
+            else if (thisBtn.Text == "#")
+            {
+                thisBtn.Text = ".";
+            }
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            Button thisBtn = (Button)sender;
+            if (thisBtn.Text == ".")
+            {
+                thisBtn.Text = "#";
+            }
+            else if (thisBtn.Text == "#")
+            {
+                thisBtn.Text = ".";
+            }
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            Button thisBtn = (Button)sender;
+            if (thisBtn.Text == ".")
+            {
+                thisBtn.Text = "#";
+            }
+            else if (thisBtn.Text == "#")
+            {
+                thisBtn.Text = ".";
+            }
+        }
+
+        private void button25_Click(object sender, EventArgs e)
+        {
+            Button thisBtn = (Button)sender;
+            if (thisBtn.Text == ".")
+            {
+                thisBtn.Text = "#";
+            }
+            else if (thisBtn.Text == "#")
+            {
+                thisBtn.Text = ".";
+            }
+        }
+
+        private void button24_Click(object sender, EventArgs e)
+        {
+            Button thisBtn = (Button)sender;
+            if (thisBtn.Text == ".")
+            {
+                thisBtn.Text = "#";
+            }
+            else if (thisBtn.Text == "#")
+            {
+                thisBtn.Text = ".";
+            }
+        }
+
+        private void button23_Click(object sender, EventArgs e)
+        {
+            Button thisBtn = (Button)sender;
+            if (thisBtn.Text == ".")
+            {
+                thisBtn.Text = "#";
+            }
+            else if (thisBtn.Text == "#")
+            {
+                thisBtn.Text = ".";
+            }
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            Button thisBtn = (Button)sender;
+            if (thisBtn.Text == ".")
+            {
+                thisBtn.Text = "#";
+            }
+            else if (thisBtn.Text == "#")
+            {
+                thisBtn.Text = ".";
+            }
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            Button thisBtn = (Button)sender;
+            if (thisBtn.Text == ".")
+            {
+                thisBtn.Text = "#";
+            }
+            else if (thisBtn.Text == "#")
+            {
+                thisBtn.Text = ".";
+            }
+        }
+
+        private void button26_Click(object sender, EventArgs e)
+        {
+            string result = "";
+            List<string> imageList = new List<string>();
+            imageList.Add(button1.Text);
+            imageList.Add(button2.Text);
+            imageList.Add(button3.Text);
+            imageList.Add(button4.Text);
+            imageList.Add(button5.Text);
+            imageList.Add(button6.Text);
+            imageList.Add(button7.Text);
+            imageList.Add(button8.Text);
+            imageList.Add(button9.Text);
+            imageList.Add(button10.Text);
+            imageList.Add(button11.Text);
+            imageList.Add(button12.Text);
+            imageList.Add(button13.Text);
+            imageList.Add(button14.Text);
+            imageList.Add(button15.Text);
+            imageList.Add(button16.Text);
+            imageList.Add(button17.Text);
+            imageList.Add(button18.Text);
+            imageList.Add(button19.Text);
+            imageList.Add(button20.Text);
+            imageList.Add(button21.Text);
+            imageList.Add(button22.Text);
+            imageList.Add(button23.Text);
+            imageList.Add(button24.Text);
+            imageList.Add(button25.Text);
+
+            result += "basic.showLeds(`\n";
+            for (int j = 0; j != 5; j++)
+            {
+                for (int i = j*5; i < j*5+5; i++)
+                {
+                    result += imageList[i];
+                }
+                result += "\n";
+            }
+            result += "`)";
+            richTextBox1.Text = result;
+        }
+    }
+}
